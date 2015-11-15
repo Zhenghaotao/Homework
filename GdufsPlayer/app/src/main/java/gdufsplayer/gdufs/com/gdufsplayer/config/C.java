6a662e640730f7d3a2d47088eddc2492a08b1145
Zhenghaotao <zhenghaotao@foxmail.com>
2015-11-16 07:53:47 +0800

    splash界面完成

---
+++ GdufsPlayer/app/src/main/java/gdufsplayer/gdufs/com/gdufsplayer/config/C.java
@@ -0,0 +1,26 @@
+package gdufsplayer.gdufs.com.gdufsplayer.config;
+
+/**
+ * Created by taotao on 15-11-15.
+ * 欢迎界面
+ */
+public class C {
+    /**
+     * 是否开启调试模式
+     */
+    public static final boolean DEVELOPER_MODE = true;
+
+
+    /**
+     * 广播action常量
+     */
+    static final class Action{
+        String action = "com.gdufs.gdusfplayer.xxx";
+    }
+
+
+
+
+
+
+}
